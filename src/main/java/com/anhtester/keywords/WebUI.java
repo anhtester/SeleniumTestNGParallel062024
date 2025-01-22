c2132342a5a2e326af47dbcbab440d6916aa55a9
An Vo <thaian.it15@gmail.com>
2025-01-22 23:08:55 +0700

    Handle Upload File

--- src/main/java/com/anhtester/keywords/WebUI.java
+++ src/main/java/com/anhtester/keywords/WebUI.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.time.Duration;
 import java.util.List;
@@ -24,6 +25,40 @@ public class WebUI {
     private static int TIMEOUT = Integer.parseInt(PropertiesHelper.getValue("EXPLICIT_WAIT"));
     private static double STEP_TIME = 0;
     private static int PAGE_LOAD_TIMEOUT = 30;
+
+    public static void uploadFileWithRobotClass(By elementFileForm, String filePath) {
+        //Click để mở form upload
+        WebUI.clickElement(elementFileForm);
+        WebUI.sleep(2);
+
+        // Khởi tạo Robot class
+        Robot rb = null;
+        try {
+            rb = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        // Copy File path vào Clipboard
+        StringSelection str = new StringSelection(filePath);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
+
+        // Nhấn Control+V để dán
+        rb.keyPress(KeyEvent.VK_CONTROL);
+        rb.keyPress(KeyEvent.VK_V);
+
+        // Xác nhận Control V trên
+        rb.keyRelease(KeyEvent.VK_CONTROL);
+        rb.keyRelease(KeyEvent.VK_V);
+
+        WebUI.sleep(1);
+
+        // Nhấn Enter
+        rb.keyPress(KeyEvent.VK_ENTER);
+        rb.keyRelease(KeyEvent.VK_ENTER);
+
+        WebUI.sleep(2);
+    }
 
     //Wait for Element
 
---
+++ src/test/java/com/anhtester/handle_UploadFile/HandleUploadFile.java
@@ -0,0 +1,55 @@
+package com.anhtester.handle_UploadFile;
+
+import com.anhtester.common.BaseTest;
+import com.anhtester.helpers.SystemHelper;
+import com.anhtester.keywords.WebUI;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class HandleUploadFile extends BaseTest {
+    @Test
+    public void testUploadFileWithSendKeys() {
+        WebUI.openURL("https://the-internet.herokuapp.com/upload");
+
+        WebUI.waitForPageLoaded();
+        WebUI.sleep(2);
+
+        By inputFileUpload = By.xpath("//input[@id='file-upload']");
+
+        //String filePath = SystemHelper.getCurrentDir() + "src\\test\\resources\\testdata\\image2.jpeg";
+
+        //Dùng File.separator để tự động thay thế dấu phân cách đường dẫn trên hệ điều hành
+        String filePath2 = SystemHelper.getCurrentDir() + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "testdata" + File.separator + "image2.jpeg";
+
+        WebUI.setText(inputFileUpload, filePath2);
+
+        WebUI.sleep(2);
+        WebUI.clickElement(By.xpath("//input[@id='file-submit']"));
+        WebUI.waitForPageLoaded();
+        Assert.assertTrue(WebUI.checkElementExist(By.xpath("//h3[normalize-space()='File Uploaded!']")), "Can not upload file.");
+    }
+
+    @Test
+    public void testUploadFileWithRobotClass() {
+        WebUI.openURL("https://files.fm/");
+        //WebUI.waitForPageLoaded();
+        WebUI.sleep(3);
+
+        By textOnPage = By.xpath("//div[@id='file_select_dragndrop_text']");
+        By divFileUpload = By.xpath("//div[@id='uploadifive-file_upload']");
+        By inputFileUpload = By.xpath("//div[@id='file_select_button']//input[@id='file_upload']");
+
+        String filePath = SystemHelper.getCurrentDir() + "src\\test\\resources\\testdata\\image1.jpg";
+
+        WebUI.uploadFileWithRobotClass(divFileUpload, filePath);
+
+        //Verify file đã upload thành công
+        By fileNameAfterUploadSuccess = By.xpath("//span[@class='filename']");
+
+        Assert.assertTrue(WebUI.checkElementExist(fileNameAfterUploadSuccess), "Can not upload file.");
+        WebUI.assertEquals(WebUI.getElementText(fileNameAfterUploadSuccess), "image1.png", "The file name not match.");
+    }
+}
